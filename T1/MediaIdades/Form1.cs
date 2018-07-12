6f88767aef630cd0588b40c68e93daf1fa514f57
v001 <v001@benner.com.br>
2018-07-12 14:44:12 -0300

    Media de idades

---
+++ T1/MediaIdades/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MediaIdades
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int idadeBob = 18;
+            int idadeKlaus = 17;
+            int idadeZe = 20;
+            double mediaIdades = ((idadeBob + idadeKlaus + idadeZe) / 3);
+            MessageBox.Show("A média das idades é : " + mediaIdades);
+
+        }
+    }
+}
