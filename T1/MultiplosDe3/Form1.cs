dac3852172651679ae46a8580852c095b444ba7e
v001 <v001@benner.com.br>
2018-07-12 14:44:46 -0300

    Lista dos multiplos de 3 entre 1 e 100

---
+++ T1/MultiplosDe3/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MultiplosDe3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String multiplos = " ";
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    multiplos += i + ", ";
+                }
+            }
+            MessageBox.Show("Os multiplos de 3 entre 1 e 100 são: " + multiplos);
+        }
+    }
+}
