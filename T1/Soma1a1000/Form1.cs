3c8f47e895c71084f257f0b8fc61b3bcc6ab7dd5
v001 <v001@benner.com.br>
2018-07-12 14:45:09 -0300

    Soma dos numeros de 1 a 1000

---
+++ T1/Soma1a1000/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Soma1a1000
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int n = 1;
+            for (int i = 0; i <= 1000; i++)
+            {
+                n += i;
+            }
+            MessageBox.Show("O resultado é: " + n);
+        }
+    }
+}
