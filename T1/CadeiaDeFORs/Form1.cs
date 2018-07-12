353ded8eb37c83cf18276e46e48e6a9b72f62f65
v001 <v001@benner.com.br>
2018-07-12 14:39:48 -0300

    For encadeados

---
+++ T1/CadeiaDeFORs/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CadeiaDeFORs
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            String texto = "";
+
+            for (int i = 1; i <= 5; i++)
+            {
+                for (int i2 = 1; i2 <= i; i2++)
+                {
+                    texto += (i * i2) + " ";
+                }
+                texto += "\n";
+            }
+            MessageBox.Show(texto);
+        }
+    }
+}
