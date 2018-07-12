55cdbac5911ce10a08f5873610ee9d1abee86db2
v001 <v001@benner.com.br>
2018-07-12 14:41:40 -0300

    Fatorial

---
+++ T1/Fatorial/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fatorial
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int f = 1;
+
+            for (int i = 1; i <= 10; i++)
+            {
+                f *= i;
+                MessageBox.Show("O fatorial de " + i + " é " + f);
+            }
+        }
+    }
+}
