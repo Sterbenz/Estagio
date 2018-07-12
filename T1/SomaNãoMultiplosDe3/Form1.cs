954927bf842b0f02eb4a7a7744327092420ddec7
v001 <v001@benner.com.br>
2018-07-12 14:45:37 -0300

    Soma de numeros que  não são multiplos de 3 entre 1 e 100

---
+++ T1/SomaNãoMultiplosDe3/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SomaNãoMultiplosDe3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int res = 0;
+            for (int i = 0; i <= 100; i++)
+            {
+                if (i % 3 != 0)
+                {
+                    res += i;
+                }
+            }
+            MessageBox.Show("Soma dos números que não são multiplos de 3 entre 1 e 100: " + res);
+        }
+    }
+}
