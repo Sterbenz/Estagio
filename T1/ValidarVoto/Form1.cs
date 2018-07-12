3452d62a003338d96458bf558bf36930cba83ebb
v001 <v001@benner.com.br>
2018-07-12 14:45:53 -0300

    Validação de votos

---
+++ T1/ValidarVoto/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ValidarVoto
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int idade = 17;
+            bool brasileiro = true;
+
+            if (idade >= 16 && brasileiro)
+            {
+                MessageBox.Show("Você está apto para votar");
+            }
+            else
+            {
+                MessageBox.Show("Você não está apto para votar");
+            }
+        }
+    }
+}
