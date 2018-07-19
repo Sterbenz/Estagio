ef06941551e089f6c0477abb541bc4ebab916b26
v001 <v001@benner.com.br>
2018-07-19 15:18:31 -0300

    Resolvendo erro no editor

--- T1/Editor/Form1.Designer.cs
+++ T1/Editor/Form1.Designer.cs
@@ -58,7 +58,7 @@
             this.Controls.Add(this.tbTexto);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
+            
             this.ResumeLayout(false);
             this.PerformLayout();
 
