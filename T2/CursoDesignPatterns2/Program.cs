d61477de77a8056747e4490cc1d71ea1c7e8fa11
v001 <v001@benner.com.br>
2018-08-16 14:30:04 -0300

    Cap1

---
+++ T2/CursoDesignPatterns2/Program.cs
@@ -0,0 +1,21 @@
+﻿using CursoDesignPatterns2.Cap1;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoDesignPatterns2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IDbConnection conexao = new ConnectionFactory().GetConnection();
+
+            IDbCommand comando = conexao.CreateCommand();
+            comando.CommandText = "Drop database";
+        }
+    }
+}
