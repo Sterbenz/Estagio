30cbcb649e92586f27faafb5e50478151c90f871
v001 <v001@benner.com.br>
2018-08-21 16:44:39 -0300

    Cap5  e finalização do curso SOLID

---
+++ T2/CursoSOLID/Cap5/ContaEstudante.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoSOLID.Cap5
+{
+    class ContaEstudante : ContaComum
+    {
+        private ManipuladorDeSaldo m;
+        public int Milhas { get; private set; }
+
+        public ContaEstudante()
+        {
+            m = new ManipuladorDeSaldo();
+        }
+        public void Deposita(double valor)
+        {
+            m.Deposita(valor);
+            this.Milhas += (int)valor;
+        }
+    }
+}
--- T2/CursoSOLID/Program.cs
+++ T2/CursoSOLID/Program.cs
@@ -1,4 +1,5 @@
 ﻿using CursoSOLID.Cap3;
+using CursoSOLID.Cap5;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,11 +12,38 @@ namespace CursoSOLID
     {
         static void Main(string[] args)
         {
-            Compra compra = new Compra(500, "Sao Paulo");
-            CalculadoraDePrecos calc = new CalculadoraDePrecos(new Frete(), new TabelaDePrecoPadrao());
+            IList<ContaComum> contas = ContasDoBanco();
 
-            Console.WriteLine("Valor da compra: " + calc.Calcula(compra));
-            Console.ReadKey();
+            foreach (ContaComum conta in contas)
+            {
+                conta.SomaInvestimento();
+
+                Console.WriteLine("Novo saldo: " + conta.Saldo);
+            }
+
+            Console.ReadLine();
+        }
+        private static IList<ContaComum> ContasDoBanco()
+        {
+            List<ContaComum> contas = new List<ContaComum>();
+            contas.Add(UmaContaComum(100));
+            contas.Add(UmaContaComum(150));
+            contas.Add(UmaContaEstudante(100));
+            return contas;
+        }
+
+        private static ContaEstudante UmaContaEstudante(double saldo)
+        {
+            ContaEstudante conta = new ContaEstudante();
+            conta.Deposita(saldo);
+            return conta;
+        }
+
+        private static ContaComum UmaContaComum(double saldo)
+        {
+            ContaComum conta = new ContaComum();
+            conta.Deposita(saldo);
+            return conta;
         }
     }
 }
